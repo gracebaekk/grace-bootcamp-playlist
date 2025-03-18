397aa31acace9d9855083c8879c3ca2cb30f11e0
gracebaekk <gracebaek@berkeley.edu>
2025-03-17 21:49:59 -0700

    Added dynamic playlist page

--- my-app/src/components/SongList.tsx
+++ my-app/src/components/SongList.tsx
@@ -1,21 +1,21 @@
 import {Playlist} from "@/types/playlist";
-import Song from "@/components/Song";
+import Songs from "@/components/Songs";
 
 export default function SongList({playlist}: { playlist: Playlist }) {
     return (
         <div className="flex flex-col gap-5">
             <hr className="border-gray-600"></hr>
             <h2 className="text-white font-bold text-xl">Songs</h2>
-            <div className="flex flex-row text-gray-400 font-lg font-bold gap-10">
-                <ul>#</ul>
-                <ul>TITLE</ul>
-                <ul>ALBUM</ul>
-                <ul>DURATION</ul>
+            <div className="grid grid-cols-12 text-gray-400 font-bold">
+                <ul className="col-span-1 ml-4">#</ul>
+                <ul className="col-span-4">TITLE</ul>
+                <ul className="col-span-5 text-right">ALBUM</ul>
+                <ul className="col-span-2 text-right">DURATION</ul>
             </div>
             <hr className="border-gray-600"></hr>
-            <div>
+            <div className="flex flex-col gap-5">
                 {playlist.songs.map((song, index) => (
-                    <Song key={song.id} song={song} index={index} />
+                    <Songs key={index} song={song} index={index + 1} />
                     ))}
             </div>
         </div> 
